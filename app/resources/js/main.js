5547ae57c05ec2142014246ea24f48a5bf12b21c
Steve Gossip <stephengossip@gmail.com>
2020-02-08 17:34:59 +0000

    added two ul's under the input header in order to house the todo and completed tasks. Each <li> is currently styled to house text on the left and two buttons on the right. The remove button is now included. Also added some basic js code to main.js in order to listen for click events on the header button.

---
+++ app/resources/js/main.js
@@ -0,0 +1,8 @@
+// User clicked on the add button
+// If there is text in the input field it shoudl be added to todo list
+ document.getElementById('add').addEventListener('click', function() {
+   var value = document.getElementById('item').value;
+   if (value) {
+     
+   }
+ });
