7a13b133fdfe0b54c2769de48d49bf9835c35446
Steve Gossip <stephengossip@gmail.com>
2020-02-11 13:40:44 +0000

    added event listener for the enter key, conditional on there being a value in the input. I also created a additem function to make the code more DRY and changed addItemToDo to addItemToDOM.

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -3,6 +3,7 @@ var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem(
   todo: [],
   completed: []
 };
+console.log(data);
 
 // Remove and complete icons in SVG format
 var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
@@ -10,30 +11,41 @@ var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:x
 
 // Called after SVG variables as they were coming back undefined
 renderToDoList();
+console.log(data);
+
 // User clicked on the add button
 // If there is text in the input field it shoudl be added to todo list
-document.getElementById('add').addEventListener('click',    function() {
+document.getElementById('add').addEventListener('click', function() {
     var value = document.getElementById('item').value;
-    if (value) addItemToDo(value);
-    // resets the input box to the placeholder text
-    document.getElementById('item').value = '';
-    data.todo.push(value);
-    dataObjectUpdated();
-    console.log(data);
+    if (value) addItem(value);
   });
 
+document.getElementById('item').addEventListener('keydown', function (event) {
+  let value = this.value;
+  if ( event.code === "Enter" && value ) {
+    addItem(value);
+  }
+})
+
+function addItem(value) {
+  addItemToDOM(value);
+  // resets the input box to the placeholder text
+  document.getElementById('item').value = '';
+  data.todo.push(value);
+  dataObjectUpdated();
+}
 
 function renderToDoList() {
   if (!data.todo.length && !data.completed.length) return;
 
   for (let i=0; i < data.todo.length; i++) {
     var value = data.todo[i];
-    addItemToDo(value);
+    addItemToDOM(value);
   }
 
   for (let j=0; j < data.completed.length; j++) {
     var value = data.completed[j];
-    addItemToDo(value, true);
+    addItemToDOM(value, true);
   }
 }
 
@@ -53,10 +65,8 @@ function removeItem() {
     data.completed.splice(data.completed.indexOf(value), 1);
   }
   // Parent is the ul for todo list and item is the individiual li
-  parent.removeChild(item);
   dataObjectUpdated();
-  console.log(data);
-
+  parent.removeChild(item);
 }
 
 function completeItem() {
@@ -71,8 +81,8 @@ function completeItem() {
   } else {
     data.completed.splice(data.completed.indexOf(value), 1);
     data.todo.push(value);
-    dataObjectUpdated();
   }
+  dataObjectUpdated();
 
 
 
@@ -84,7 +94,7 @@ function completeItem() {
 }
 
 // Add new item to the todo list
-function addItemToDo(text, completed) {
+function addItemToDOM(text, completed) {
   var list = (completed) ? document.getElementById('completed') : document.getElementById('todo');
 
   var item = document.createElement('li');
